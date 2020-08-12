7649dc26a15e8319eb8cb50609ff2225d9a37f2d
AaronIyrene <aaronpsam@gmail.com>
2020-08-12 22:40:09 +0530

    modified

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome to React Native.</Text>
+      <Text>This is a good app.</Text>
       <StatusBar style="auto" />
     </View>
   );
